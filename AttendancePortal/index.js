8cddcc3a2aaabce5d5cd9ca31af1b1789102dbc0
yaarAdarsh <addudx@gmail.com>
2025-03-12 14:51:22 +0530

    WelcomeToHood

---
+++ AttendancePortal/index.js
@@ -0,0 +1,75 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let mood;
+let myPoem;
+
+app.get("/", (req, res) => {
+ 
+  res.render("index.ejs");  
+});
+
+app.post("/submit", async(req, res) => {
+  try {
+    const response = await axios.post(`http://127.0.0.1:5000/emotion_detect`);
+    const result = response.data;
+    const yoyo=result.link;
+    mood = result.result;
+    console.log(yoyo);
+    console.log(mood);
+    if(mood==="Anger" || mood === "Fear"  ||mood==="Sad")
+    {
+      let num = Math.floor(Math.random()*7);
+      myPoem = poetry[num];
+    }
+    else if(mood === "Happy" || mood === "Surprise" || mood ==="Neutral") {
+      let num = Math.floor(Math.random() * (14 - 8 + 1)) + 8;
+      myPoem = poetry[num];
+    }
+    res.render("index.ejs", { poem: myPoem, mood: mood, photo: yoyo, son: songs });
+    console.log(result);
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "No activities that match your criteria.",
+    });
+  }
+  // res.render("index.ejs", {randomName:randomName, poem: poetry[0]});
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+const poetry = [
+  "तू रुका नहीं, तू गिरा है तो फिर उठ, अभी हार मत मान, तेरे पास वो हिम्मत है जो सबसे बड़ी है।", 
+  "वक़्त की घड़ी कभी रुकती नहीं,हर टूटा हुआ सपना फिर से संजीवित हो सकता है।",
+  "संगीन रातें भी एक दिन खत्म होती हैं, तेरे हर आंसू के बाद मुस्कान फिर लौट आती है।",
+  "हर दर्द के बाद दिल में एक नई उमंग उठती है, तुम फिर से उठकर अपने सपनों को सच कर सकते हो।",
+  "अंधेरे में छुपा सूरज अब भी है वहीं, तुम खुद से कहो, ‘मैं कभी हार नहीं मानूंगा।’",
+  "तेरी कामयाबी पर तारीफ़, तेरी कोशिश पर ताना होगा, तेरे दुख में कुछ लोग, तेरे सुख में ज़माना होगा।",
+  "हसरत ऐ ज़िंदगी हमारा ये उसूल है, अगर ख़्वाहिश है चाँद की तो दाग भी क़बूल है।",
+  "तेरी सुबह के सूरज में कुछ खास बात है, जो तेरे चेहरे पे मुस्कान की बरसात है।",
+  "तेरे दिन की शुरुआत भी जैसे एक नया सफर हो, हर कदम में खुशी का जादू और रंगों का असर हो।",
+  "तू जब उठता है, तो हर सुबह रोशन होती है, तेरी हंसी से दुनिया खुद भी खुश होती है।",
+  "तेरी मेहनत में वो चमक है, जो दिन को रोशन कर दे, तेरे हर पल में एक नई उम्मीद खिल जाए।",
+  "तेरे दिन की शुरुआत से ही, ख़ुशियाँ फैली रहती हैं, तेरे हर कदम से दुनिया में कुछ खास बात रहती है।",
+  "जब तुम उठ खड़े होते हो, तो धरती भी थामती है कदम, सपनों को जीने का हौंसला हो, तो कोई भी रुकावट नहीं दम।",
+  "न रुको, न थको, बस चलते रहो, सपने पूरे होंगे, यकीन रखो।" 
+];
+
+const songs = [
+  {name: "Kya hua tera vada", link: "https://www.youtube.com/watch?v=Z5D1dhTMclI"},
+  {name: "Aa chal ke tujhe", link: "https://www.youtube.com/watch?v=Y_8VmzWOsgs"},
+  {name: "Mai Jindagi ka Saath", link: "https://www.youtube.com/watch?v=ivBhqDtJeiw"}
+];
+
+ 
---
+++ server/app.py
@@ -0,0 +1,65 @@
+from flask import Flask,jsonify
+import numpy as np
+import cv2
+from keras.models import load_model
+from flask_cors import CORS
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+info = {}
+
+haarcascade = "haarcascade_frontalface_default.xml"
+label_map = ['Anger', 'Neutral', 'Fear', 'Happy', 'Sad', 'Surprise']
+print("+"*50, "loadin gmmodel")
+model = load_model(r"E:\minorModel\myModel\server\model.h5")
+cascade = cv2.CascadeClassifier(haarcascade)
+
+@app.route('/emotion_detect', methods=["POST"])
+def emotion_detect():
+	
+	found = False 
+
+	cap = cv2.VideoCapture(0)
+	while not(found):
+		_, frm = cap.read()
+  
+		timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+		image_filename = f"yourpic_{timestamp}.jpg"
+		cv2.imwrite(os.path.join("static/img", image_filename), frm)
+  
+		gray = cv2.cvtColor(frm,cv2.COLOR_BGR2GRAY)
+
+		faces = cascade.detectMultiScale(gray, 1.4, 1)
+
+		for x,y,w,h in faces:
+			found = True
+			roi = gray[y:y+h, x:x+w]
+			cv2.imwrite("static/face.jpg", roi)
+
+	roi = cv2.resize(roi, (48,48))
+
+	roi = roi/255.0
+	
+	roi = np.reshape(roi, (1,48,48,1))
+
+	prediction = model.predict(roi)
+
+	print(prediction)
+	
+	prediction = np.argmax(prediction)
+	prediction = label_map[prediction]
+
+	cap.release()
+	link = f"http://127.0.0.1:5000/static/img/{image_filename}"
+	return jsonify({'result': prediction,'link':link})
+	
+
+if __name__ == "__main__":
+	
+	# CORS(app, origins="http://localhost:5173")  #Frontend server linked 
+	app.run(debug=True)
+ 
